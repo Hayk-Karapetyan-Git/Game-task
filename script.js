05d802572e3ef7ad3575f26e61cb552db3cf1fd4
Hayk <114661149+Hayk-Karapetyan-Git@users.noreply.github.com>
2022-10-04 18:54:25 +0400

    Update Script

--- script.js
+++ script.js
@@ -1,199 +1,253 @@
-document.addEventListener('DOMContentLoaded', () => {
-  
-  const width = 28
-  const grid = document.querySelector('.grid')
+const button = document.getElementById("btn");
+button.addEventListener("click", () => {
+  const width = 28;
+  const grid = document.querySelector("#root");
   const layout = [
-    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
-    1,1,4,4,4,4,4,4,4,4,4,4,4,1,1,4,4,4,4,4,4,4,4,4,4,4,4,1,
-    1,4,1,1,1,1,4,1,1,1,1,1,4,1,1,4,1,1,1,1,1,4,1,1,1,1,4,1,
-    1,4,1,1,1,1,4,1,1,1,1,1,4,1,1,4,1,1,1,1,1,4,1,1,1,1,4,1,
-    1,4,1,1,1,1,4,1,1,1,1,1,4,1,1,4,1,1,1,1,1,4,1,1,1,1,4,1,
-    1,4,4,4,4,4,4,4,1,4,4,4,4,4,4,4,1,4,4,4,4,4,4,4,4,4,4,1,
-    1,4,1,1,1,1,4,1,1,4,4,4,1,1,1,1,1,4,4,4,1,4,1,1,1,1,4,1,
-    1,4,1,1,1,1,4,1,1,4,4,4,4,4,4,4,4,4,4,4,1,4,1,1,1,1,4,1,
-    1,4,4,4,4,4,4,4,4,4,4,4,4,1,1,4,4,4,4,4,4,4,4,4,4,4,4,1,
-    1,1,1,1,1,1,4,1,1,1,1,1,4,1,1,1,1,1,1,1,1,4,1,4,4,4,1,1,
-    1,1,1,1,1,1,4,1,1,4,4,4,4,4,4,1,4,4,4,1,1,4,1,4,4,4,1,1,
-    1,1,1,1,1,1,4,1,1,4,1,1,1,4,4,1,1,4,4,1,1,4,1,4,4,4,1,1,
-    1,1,1,1,1,1,4,1,1,4,1,4,4,4,4,4,4,4,4,1,1,4,1,4,4,4,1,1,
-    1,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,1,4,4,4,4,4,4,4,4,4,1,
-    1,1,1,1,1,1,4,1,1,4,1,4,4,4,4,4,4,1,4,1,1,1,1,1,1,1,1,1,
-    1,1,1,1,1,1,4,1,1,4,1,1,4,4,4,1,1,1,4,1,1,4,1,1,1,1,1,1,
-    1,1,1,1,1,1,4,1,1,4,1,1,4,4,4,1,1,1,4,1,1,4,1,1,1,1,1,1,
-    1,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,1,
-    1,4,1,1,1,1,4,1,1,1,1,1,4,1,1,4,1,1,1,1,1,4,1,1,1,1,4,1,
-    1,4,1,1,1,1,4,1,1,1,1,1,4,1,1,4,1,1,1,1,1,4,1,1,1,1,4,1,
-    1,4,4,4,1,1,4,4,4,4,4,4,4,4,4,4,1,4,4,4,4,4,1,1,4,4,4,1,
-    1,1,1,4,1,1,4,1,1,4,1,1,1,1,1,1,1,1,4,1,1,4,1,1,4,1,1,1,
-    1,1,1,4,1,1,4,1,1,4,1,1,1,1,1,1,1,1,4,1,1,4,1,1,4,1,1,1,
-    1,4,4,4,4,4,4,1,1,4,4,4,4,1,1,4,4,4,4,1,1,4,4,4,4,4,4,1,
-    1,4,1,1,1,4,4,4,4,4,1,1,4,1,1,4,1,1,1,1,1,1,1,1,1,1,4,1,
-    1,4,1,1,1,4,4,4,4,4,1,1,4,1,1,4,1,1,1,1,1,1,1,1,1,1,4,1,
-    1,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,1,
-    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
-  ]
-  
-  
-  const Iterators = []
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 4, 1, 1, 4, 1, 1, 1,
+    1, 1, 4, 1, 1, 1, 1, 4, 4, 4, 4, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 4, 1, 1, 4,
+    1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 4, 4, 4, 4, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 4,
+    1, 1, 4, 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 4,
+    4, 4, 4, 4, 4, 4, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 1, 1, 4,
+    1, 1, 4, 4, 4, 1, 1, 1, 1, 1, 4, 4, 4, 1, 4, 1, 1, 1, 1, 4, 4, 4, 4, 1, 1,
+    1, 1, 4, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 4, 1, 1, 1, 1, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1,
+    4, 4, 4, 1, 4, 4, 1, 1, 1, 1, 1, 4, 1, 1, 4, 4, 4, 4, 4, 4, 1, 4, 4, 4, 1,
+    1, 4, 1, 4, 4, 4, 1, 4, 4, 1, 1, 1, 1, 1, 4, 1, 1, 4, 1, 1, 1, 4, 4, 1, 1,
+    4, 4, 1, 1, 4, 1, 4, 4, 4, 1, 4, 4, 1, 1, 1, 1, 1, 4, 1, 1, 4, 1, 4, 4, 4,
+    4, 4, 4, 4, 4, 1, 1, 4, 1, 4, 4, 4, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1, 1, 1, 1, 4, 1,
+    1, 4, 1, 4, 4, 4, 4, 4, 4, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 4, 4, 1, 1, 1, 1,
+    1, 4, 1, 1, 4, 1, 1, 4, 4, 4, 1, 1, 1, 4, 1, 1, 4, 1, 1, 1, 1, 1, 4, 4, 1,
+    1, 1, 1, 1, 4, 1, 1, 4, 1, 1, 4, 4, 4, 1, 1, 1, 4, 1, 1, 4, 1, 1, 1, 1, 1,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 4, 1, 1, 4, 1, 1, 1, 1, 1,
+    4, 1, 1, 1, 1, 4, 4, 4, 4, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 4, 1, 1, 4, 1, 1,
+    1, 1, 1, 4, 1, 1, 1, 1, 4, 4, 4, 4, 4, 4, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 1, 4, 4, 4, 4, 4, 1, 1, 4, 4, 4, 4, 4, 1, 1, 4, 1, 1, 4, 1, 1, 4, 1, 1,
+    1, 1, 1, 1, 1, 1, 4, 1, 1, 4, 1, 1, 4, 1, 1, 4, 4, 1, 1, 4, 1, 1, 4, 1, 1,
+    4, 1, 1, 1, 1, 1, 1, 1, 1, 4, 1, 1, 4, 1, 1, 4, 1, 1, 4, 4, 4, 4, 4, 4, 4,
+    4, 1, 1, 4, 4, 4, 4, 1, 1, 4, 4, 4, 4, 1, 1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1,
+    1, 1, 4, 4, 4, 4, 4, 1, 1, 4, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 4, 4,
+    4, 4, 1, 1, 1, 4, 4, 4, 4, 4, 1, 1, 4, 1, 1, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,
+    4, 4, 4, 4, 4, 4, 4, 4, 4,
+  ];
+
+  const Iterators = [];
   function createMap() {
     for (let i = 0; i < layout.length; i++) {
-      const it = document.createElement('div')
-      grid.appendChild(it)
-      Iterators.push(it)
-    if (layout[i] === 1) {
-        Iterators[i].classList.add('wall')
-      } 
+      const it = document.createElement("div");
+      grid.appendChild(it);
+      Iterators.push(it);
+      if (layout[i] === 1) {
+        Iterators[i].classList.add("wall");
+      }
     }
   }
-  createMap()
+  createMap();
 
-  let rabbitCurrentIndex = 485
-  Iterators[rabbitCurrentIndex].classList.add('rabbit')
-  Iterators[rabbitCurrentIndex].innerHTML = '🐰'
+  let rabbitCurrentIndex = Math.trunc(Math.random() * 784);
 
+  while (
+    Iterators[rabbitCurrentIndex].classList.contains("wolf") ||
+    Iterators[rabbitCurrentIndex].classList.contains("wall") ||
+    Iterators[rabbitCurrentIndex].classList.contains("home")
+  ) {
+    rabbitCurrentIndex = Math.trunc(Math.random() * 784);
+  }
 
-  let homeCurrentIndex = 53
-  Iterators[homeCurrentIndex].classList.add('home')
-  Iterators[homeCurrentIndex].innerHTML = '🏡'
+  Iterators[rabbitCurrentIndex].classList.add("rabbit");
+  Iterators[rabbitCurrentIndex].innerHTML = "🐰";
+
+  let homeCurrentIndex = 53;
+  Iterators[homeCurrentIndex].classList.add("home");
+  Iterators[homeCurrentIndex].innerHTML = "🏡";
 
   function moveRabbit(e) {
-    Iterators[rabbitCurrentIndex].classList.remove('rabbit')
-    Iterators[rabbitCurrentIndex].innerHTML = ''
-    switch(e.keyCode) {
+    Iterators[rabbitCurrentIndex].classList.remove("rabbit");
+    Iterators[rabbitCurrentIndex].innerHTML = "";
+    switch (e.keyCode) {
       case 37:
-        if(
+        if (
           rabbitCurrentIndex % width !== 0 &&
-          !Iterators[rabbitCurrentIndex -1].classList.contains('wall')
-          )
-        rabbitCurrentIndex -= 1
-        if (Iterators[rabbitCurrentIndex -1] === Iterators[363]) {
-          rabbitCurrentIndex = 391
+          !Iterators[rabbitCurrentIndex - 1].classList.contains("wall")
+        ) {
+          rabbitCurrentIndex -= 1;
+        } else if (rabbitCurrentIndex % width == 0) {
+          rabbitCurrentIndex += width - 1;
         }
-  wolfes.forEach(wolf => moveWolf(wolf))
+        wolfes.forEach((wolf) => moveWolf(wolf));
 
-        break
+        break;
       case 38:
-        if(
+        if (
           rabbitCurrentIndex - width >= 0 &&
-          !Iterators[rabbitCurrentIndex -width].classList.contains('wall')
-          ) 
-        rabbitCurrentIndex -= width
-  wolfes.forEach(wolf => moveWolf(wolf))
-
-        break
-      case 39:
-        if(
-          rabbitCurrentIndex % width < width - 1 &&
-          !Iterators[rabbitCurrentIndex +1].classList.contains('wall')
-        )
-        rabbitCurrentIndex += 1
-        if (Iterators[rabbitCurrentIndex +1] === Iterators[392]) {
-          rabbitCurrentIndex = 364
+          !Iterators[rabbitCurrentIndex - width].classList.contains("wall")
+        ) {
+          rabbitCurrentIndex -= width;
+        } else if (rabbitCurrentIndex - width <= 0) {
+          rabbitCurrentIndex += width * (width - 1);
         }
-  wolfes.forEach(wolf => moveWolf(wolf))
+        wolfes.forEach((wolf) => moveWolf(wolf));
 
-        break
+        break;
+      case 39:
+        if (
+          rabbitCurrentIndex % width < width - 1 &&
+          !Iterators[rabbitCurrentIndex + 1].classList.contains("wall")
+        )
+          rabbitCurrentIndex += 1;
+        if (rabbitCurrentIndex % width == width - 1) {
+          rabbitCurrentIndex -= width - 1;
+        }
+        wolfes.forEach((wolf) => moveWolf(wolf));
+
+        break;
       case 40:
         if (
           rabbitCurrentIndex + width < width * width &&
-          !Iterators[rabbitCurrentIndex +width].classList.contains('wall')
-        )
-        rabbitCurrentIndex += width
-  wolfes.forEach(wolf => moveWolf(wolf))
+          !Iterators[rabbitCurrentIndex + width].classList.contains("wall")
+        ) {
+          rabbitCurrentIndex += width;
+        } else if (rabbitCurrentIndex + width >= width * width) {
+          rabbitCurrentIndex -= width * (width - 1);
+        }
+        wolfes.forEach((wolf) => moveWolf(wolf));
 
-        break
+        break;
     }
-    Iterators[rabbitCurrentIndex].classList.add('rabbit')
-    Iterators[rabbitCurrentIndex].innerHTML = '🐰'
-    checkForGameOver()
-    checkForWin()
+    Iterators[rabbitCurrentIndex].classList.add("rabbit");
+    Iterators[rabbitCurrentIndex].innerHTML = "🐰";
+    checkForGameOver();
+    checkForWin();
   }
-  document.addEventListener('keyup', moveRabbit)
+  document.addEventListener("keyup", moveRabbit);
   class Wolf {
     constructor(className, startIndex, speed) {
-      this.className = className
-      this.startIndex = startIndex
-      this.speed = speed
-      this.currentIndex = startIndex
-      this.isScared = false
-      this.timerId = NaN
+      this.className = className;
+      this.startIndex = startIndex;
+      this.speed = speed;
+      this.currentIndex = startIndex;
+      this.isScared = false;
+      this.timerId = NaN;
     }
   }
   wolfes = [
-    new Wolf('wolf', 348, 250),
-    new Wolf('wolf', 376, 400),
-    new Wolf('wolf', 351, 300),
-    new Wolf('wolf', 379, 500)
-    ]
-  wolfes.forEach(wolf => {
-    Iterators[wolf.currentIndex].classList.add(wolf.className)
-    Iterators[wolf.currentIndex].classList.add('wolf')
-    Iterators[wolf.currentIndex].innerHTML = '🐺'
-
-    })
-
+    new Wolf("wolf", Math.trunc(Math.random() * 768), 250),
+    new Wolf("wolf", Math.trunc(Math.random() * 768), 400),
+    new Wolf("wolf", Math.trunc(Math.random() * 768), 300),
+    new Wolf("wolf", Math.trunc(Math.random() * 768), 500),
+  ];
+  wolfes.forEach((wolf) => {
+    while (
+      Iterators[wolf.currentIndex].classList.contains("wolf") ||
+      Iterators[wolf.currentIndex].classList.contains("wall") ||
+      Iterators[wolf.currentIndex].classList.contains("rabbit") ||
+      Iterators[wolf.currentIndex].classList.contains("home")
+    ) {
+      wolf.currentIndex = Math.trunc(Math.random() * 784);
+    }
+    Iterators[wolf.currentIndex].classList.add(wolf.className);
+    Iterators[wolf.currentIndex].classList.add("wolf");
+    Iterators[wolf.currentIndex].innerHTML = "🐺";
+    Iterators[homeCurrentIndex].innerHTML = "🏡";
+  });
 
   function moveWolf(wolf) {
-    const directions =  [-1, +1, width, -width]
-    let direction ;
-    let range = rabbitCurrentIndex - wolf.currentIndex
-    let wolfK = [wolf.currentIndex % width,Math.floor(wolf.currentIndex/width)]
-    let rabbitK = [rabbitCurrentIndex % width,Math.floor(rabbitCurrentIndex/width)]
-    if(range > 0){
-      if(wolfK[1] < rabbitK[1]){
-          direction = width;
-      }else{
+    const directions = [-1, +1, width, -width];
+    let direction;
+    let range = rabbitCurrentIndex - wolf.currentIndex;
+    let wolfK = [
+      wolf.currentIndex % width,
+      Math.floor(wolf.currentIndex / width),
+    ];
+    let rabbitK = [
+      rabbitCurrentIndex % width,
+      Math.floor(rabbitCurrentIndex / width),
+    ];
+    if (range > 0) {
+      if (wolfK[1] < rabbitK[1]) {
+        direction = width;
+      } else {
         direction = 1;
       }
-    }else{
-      if(wolfK[1] > rabbitK[1]){
-        direction = -width;
-      }else{
+    } else {
+      if (wolfK[1] >= rabbitK[1]) {
+        if (wolfK[0] > rabbitK[0]) {
+          direction = -1;
+        } else {
+          direction = -width;
+        }
+      } else {
         direction = -1;
-
       }
     }
 
-    if( range == width ){
+    if (range == width) {
       direction = width;
-
-    }else if(range == 1){
+    } else if (range == 1) {
       direction = 1;
-
-    }else if (range == -width){
+    } else if (range == -width) {
       direction = -width;
-
-    }else if(range == -1){
-
+    } else if (range == -1) {
       direction = -1;
     }
 
-      if  (!Iterators[wolf.currentIndex + direction].classList.contains('wolf') &&
-        !Iterators[wolf.currentIndex + direction].classList.contains('wall') ) {
-          Iterators[wolf.currentIndex].classList.remove(wolf.className)
-          Iterators[wolf.currentIndex].innerHTML = ' '
+    const move = (direcion) => {
+      if (wolf.currentIndex < width) {
+        direction == -width && (direction = -direction);
+      } else if (wolf.currentIndex % width == 0) {
+        direction == -1 && (direction = +1);
+      } else if (wolf.currentIndex % width == width - 1) {
+        direction == +1 && (direction = -1);
+      } else if (wolf.currentIndex >= width * (width - 1)) {
+        direction == +width && (direction = -width);
+      }
 
-          wolf.currentIndex += direction
-          Iterators[wolf.currentIndex].classList.add(wolf.className, 'wolf')
-          Iterators[wolf.currentIndex].innerHTML = '🐺'
+      Iterators[wolf.currentIndex].classList.remove(wolf.className);
+      Iterators[wolf.currentIndex].innerHTML = " ";
 
-      } else direction = directions[Math.floor(Math.random() * directions.length)]
-      
-    checkForGameOver()
-    
+      wolf.currentIndex += direction;
+      Iterators[wolf.currentIndex].classList.add(wolf.className, "wolf");
+      Iterators[wolf.currentIndex].innerHTML = "🐺";
+    };
+    if (
+      !Iterators[wolf.currentIndex + direction].classList.contains("wolf") &&
+      !Iterators[wolf.currentIndex + direction].classList.contains("wall")
+    ) {
+      move(direction);
+    } else {
+      direction = directions[Math.floor(Math.random() * directions.length)];
+      if (
+        !Iterators[wolf.currentIndex + direction].classList.contains("wolf") &&
+        !Iterators[wolf.currentIndex + direction].classList.contains("wall")
+      ) {
+        move(direction);
+      }
+    }
+
+    checkForGameOver();
   }
   function checkForGameOver() {
-    if (Iterators[rabbitCurrentIndex].classList.contains('wolf')) {
+    if (Iterators[rabbitCurrentIndex].classList.contains("wolf")) {
       // wolfes.forEach(wolf => clearInterval(wolf.timerId))
-      document.removeEventListener('keyup', moveRabbit)
-      setTimeout(function(){ alert("Game Over"); }, 500)
+      document.removeEventListener("keyup", moveRabbit);
+      setTimeout(function () {
+        alert("Game Over");
+      }, 500);
     }
   }
   function checkForWin() {
     if (rabbitCurrentIndex == homeCurrentIndex) {
       // wolfes.forEach(wolf => clearInterval(wolf.timerId))
-      document.removeEventListener('keyup', moveRabbit)
-      setTimeout(function(){ alert("You have WON!"); }, 500)
+      document.removeEventListener("keyup", moveRabbit);
+      setTimeout(function () {
+        alert("You have WON!");
+      }, 500);
     }
   }
-})
+});
