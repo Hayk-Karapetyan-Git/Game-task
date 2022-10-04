56b65f40ba8230ade83150dc54d7bb4b3f531a8a
Hayk <114661149+Hayk-Karapetyan-Git@users.noreply.github.com>
2022-10-04 04:37:56 +0400

    Update Script wolves direction

--- script.js
+++ script.js
@@ -69,6 +69,8 @@ document.addEventListener('DOMContentLoaded', () => {
         if (Iterators[rabbitCurrentIndex -1] === Iterators[363]) {
           rabbitCurrentIndex = 391
         }
+  wolfes.forEach(wolf => moveWolf(wolf))
+
         break
       case 38:
         if(
@@ -76,6 +78,8 @@ document.addEventListener('DOMContentLoaded', () => {
           !Iterators[rabbitCurrentIndex -width].classList.contains('wall')
           ) 
         rabbitCurrentIndex -= width
+  wolfes.forEach(wolf => moveWolf(wolf))
+
         break
       case 39:
         if(
@@ -86,6 +90,8 @@ document.addEventListener('DOMContentLoaded', () => {
         if (Iterators[rabbitCurrentIndex +1] === Iterators[392]) {
           rabbitCurrentIndex = 364
         }
+  wolfes.forEach(wolf => moveWolf(wolf))
+
         break
       case 40:
         if (
@@ -93,6 +99,8 @@ document.addEventListener('DOMContentLoaded', () => {
           !Iterators[rabbitCurrentIndex +width].classList.contains('wall')
         )
         rabbitCurrentIndex += width
+  wolfes.forEach(wolf => moveWolf(wolf))
+
         break
     }
     Iterators[rabbitCurrentIndex].classList.add('rabbit')
@@ -124,13 +132,42 @@ document.addEventListener('DOMContentLoaded', () => {
 
     })
 
-  wolfes.forEach(wolf => moveWolf(wolf))
 
   function moveWolf(wolf) {
     const directions =  [-1, +1, width, -width]
-    let direction = directions[Math.floor(Math.random() * directions.length)]
+    let direction ;
+    let range = rabbitCurrentIndex - wolf.currentIndex
+    let wolfK = [wolf.currentIndex % width,Math.floor(wolf.currentIndex/width)]
+    let rabbitK = [rabbitCurrentIndex % width,Math.floor(rabbitCurrentIndex/width)]
+    if(range > 0){
+      if(wolfK[1] < rabbitK[1]){
+          direction = width;
+      }else{
+        direction = 1;
+      }
+    }else{
+      if(wolfK[1] > rabbitK[1]){
+        direction = -width;
+      }else{
+        direction = -1;
 
-    wolf.timerId = setInterval(function() {
+      }
+    }
+
+    if( range == width ){
+      direction = width;
+
+    }else if(range == 1){
+      direction = 1;
+
+    }else if (range == -width){
+      direction = -width;
+
+    }else if(range == -1){
+
+      direction = -1;
+    }
+
       if  (!Iterators[wolf.currentIndex + direction].classList.contains('wolf') &&
         !Iterators[wolf.currentIndex + direction].classList.contains('wall') ) {
           Iterators[wolf.currentIndex].classList.remove(wolf.className)
@@ -141,29 +178,20 @@ document.addEventListener('DOMContentLoaded', () => {
           Iterators[wolf.currentIndex].innerHTML = '🐺'
 
       } else direction = directions[Math.floor(Math.random() * directions.length)]
-      if(wolf.isScared && Iterators[wolf.currentIndex].classList.contains('rabbit')) {
-        Iterators[wolf.currentIndex].classList.remove(wolf.className, 'wolf')
-        Iterators[wolf.currentIndex].innerHTML = ' '
-
-        wolf.currentIndex = wolf.startIndex
-        score +=100
-        Iterators[wolf.currentIndex].classList.add(wolf.className, 'wolf')
-        Iterators[wolf.currentIndex].innerHTML = '🐺'
-
-      }
+      
     checkForGameOver()
-    }, wolf.speed)
+    
   }
   function checkForGameOver() {
     if (Iterators[rabbitCurrentIndex].classList.contains('wolf')) {
-      wolfes.forEach(wolf => clearInterval(wolf.timerId))
+      // wolfes.forEach(wolf => clearInterval(wolf.timerId))
       document.removeEventListener('keyup', moveRabbit)
       setTimeout(function(){ alert("Game Over"); }, 500)
     }
   }
   function checkForWin() {
     if (rabbitCurrentIndex == homeCurrentIndex) {
-      wolfes.forEach(wolf => clearInterval(wolf.timerId))
+      // wolfes.forEach(wolf => clearInterval(wolf.timerId))
       document.removeEventListener('keyup', moveRabbit)
       setTimeout(function(){ alert("You have WON!"); }, 500)
     }
